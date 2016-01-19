8a97d3a077025b4f6806b02b513c84475c5114cf
Remko A. de Jong <loseyourmind@gmail.com>
2016-01-19 22:38:17 +0100

    4K

--- commands.r
+++ commands.r
@@ -2,3 +2,10 @@ git init
 git remote add origin http://github.com/rdj020/xp-repo.git
 git commit -m first1
 git push origin master
+git commit -a -m message
+git add -A
+git add -u #update
+git checkout -b branchname
+git branch # which branch am i in?
+git checkout master #Ga naar branch master
+git checkout branchname #Ga naar branch als ie al bestaat
